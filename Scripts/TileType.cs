5c431f2513dc84951187047c56b546b7f8c5e41c
Mona25Th <147948747+Mona25Th@users.noreply.github.com>
2026-02-01 23:49:36 +0100

    Create TileType.cs

---
+++ Scripts/TileType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileType", menuName = "FloodGame/TileType")]
+public class TileType : ScriptableObject
+{
+    public string tileName;
+    public Sprite sprite;
+}
